47b36006110abe013df76845c8defe95cb12ba0f
Harshal Mali <117845618+Raga0074@users.noreply.github.com>
2024-05-19 15:17:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Todoscreen from './src/screens/Todoscreen';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+    <View>
+      <Todoscreen />
+    </View>
+    </SafeAreaView>
+  );
+}
